6ffd753154fcc4c5a868018d2c05bf37bed7a109
Mercziler <53012352+Mercziler@users.noreply.github.com>
2020-09-25 19:07:21 +0000

    Update connect.php

--- connect.php
+++ connect.php
@@ -13,6 +13,9 @@
 
 
 echo $DB_DATABASE;
+echo $DB_USER;
+echo $DB_PASS;
+echo $DB_SERVER;
 	 	 try { 
 	 $connect =  new PDO("mysql:host=$DB_SERVER; dbname=$DB_DATABASE", $DB_USER,$DB_PASS); 
 	 } 
