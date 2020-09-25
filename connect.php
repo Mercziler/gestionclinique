aa50fa978096b7c808d9466d8dfe5043bf40802d
Mercziler <53012352+Mercziler@users.noreply.github.com>
2020-09-25 19:01:32 +0000

    Update connect.php

--- connect.php
+++ connect.php
@@ -10,7 +10,19 @@
 	 $DB_PASS = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
 	 $DB_DATABASE = getenv('MYSQL_DATABASE');
 /******************************************************************/
-$conn =new mysqli($DB_SERVER,$DB_USER,$DB_PASS,$DB_DATABASE) or die(mysqli_connect_error());
 
-	 	
+	 	 try { 
+	 $connect =  new PDO("mysql:host=$DB_SERVER; dbname=$DB_DATABASE", $DB_USER,$DB_PASS); 
+	 } 
+ 
+	echo $DB_DATABASE;
+	 catch (PDOException $e) { 
+
+	 	if (empty($DB_DATABASE)) {
+	 		die("<strong>Database Error..! </strong><a href='install'>Start installation</a>") ;
+	 	} else {
+	 		die("<strong>Database Error..! </strong>") ;
+	 	}
+	 	 
+	 } 
 ?>
