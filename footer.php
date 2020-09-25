1ffb2c97af0b85d1136b64d9053d71887ba4cc39
Mercziler <53012352+Mercziler@users.noreply.github.com>
2020-09-25 19:41:34 +0200

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,3 @@
+<div id="footer">
+  Copyright &copy; <?php echo date("Y"); ?> | <a href="http://www.doum-soft.net" title="cliquez sur le lien pour acceder a notre site web">Doum~soft95</a> - system de gestion d'un clinique 
+</div>   
