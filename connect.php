193617d22abb6edced2f7381b19f66923adfcc46
Mercziler <53012352+Mercziler@users.noreply.github.com>
2020-09-25 18:26:57 +0000

    Update connect.php

--- connect.php
+++ connect.php
@@ -11,22 +11,7 @@
 	 $DB_DATABASE = getenv('OPENSHIFT_GEAR_NAME');
 /******************************************************************/
  
-	 try { 
-	 $connect =  new PDO("mysql:host=$DB_SERVER; dbname=$DB_DATABASE", $DB_USER,$DB_PASS); 
-	 } 
- 
-	 catch (PDOException $e) { 
 
-	 	if (empty($DB_DATABASE)) {
-	 		die("<strong>Database Error..! </strong><a href='install'>Start installation</a>") ;
-	 	} else {
-	 		die("<strong>Database Error..! </strong>") ;
-	 	}
-	 	 
-	 } 
- 
-	 $connect->query("set charcter_set_server = 'utf8'"); 
-	 $connect->query("set names'utf8' "); 
-	 	
+	 	$conn =new mysqli($DB_SERVER,$DB_USER,$DB_PASS,$DB_DATABASE) or die(mysqli_connect_error());
  
 ?>
